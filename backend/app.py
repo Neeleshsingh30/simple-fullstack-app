59c19157b45ec736ee7b9371386ab6cf13151132
Neelesh Singh <neeleshchoursiya0786@gmail.com>
2026-09-20 02:44:32 +0530

    Initial Dockerized full stack application

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Backend is running successfully!"
+    }
+
+
+@app.get("/api/message")
+def message():
+    return {
+        "message": "Hello from FastAPI Backend!"
+    }
